ff6a9324d660d1216db754d8028f61d402c8e8c4
halodong <1661089448@qq.com>
2021-03-10 10:46:53 +0800

    优化配置

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import { Header } from './components/header'
 import { Edit } from './components/edit'
 import { Banner } from './components/banner'
+
 function App() {
   return (
     <>
       <Header />
-      <Edit />
-      <Banner/>
+      <div style={{
+        minWidth: '600px',
+        padding: '0 50px 30px 50px',
+        border: '1px solid white'
+      }}><Edit /></div>
+      <Banner />
     </>
 
   );
