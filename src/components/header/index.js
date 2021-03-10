99f00afae4e31775170339bbb2d0cac77593f80c
halodong <1661089448@qq.com>
2021-03-10 10:39:07 +0800

    样式优化

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -5,12 +5,11 @@ export const Header = () => {
     return (
         <>
             <div className='header'>
-                <div className='header-title'>ToImg</div>
+                <div className='header-title'>CodeImg</div>
                 <span>创建并分享源代码的美丽图像。</span>
                 <br />
-                <span>开始输入或将文件放入文本区域以开始。</span>
+                <span>开始输入或将文字粘贴到文本区域以开始。</span>
             </div>
-
         </>
     )
 }
