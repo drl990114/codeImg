41818ec7de30a50eb579150d711f8e184029bf3f
halodong <1661089448@qq.com>
2021-03-10 10:59:59 +0800

    优化样式

--- src/components/banner/index.js
+++ src/components/banner/index.js
@@ -5,6 +5,7 @@ export const Banner = () => {
         <div className='banner'>
             <a className='link' href='https://github.com/halodong/codeImg'>github</a>
             <a className='link' href='https://gitee.com/halodong/codeimg' >gitee</a>
+            <a className='link' href='http://halodong.gitee.io/flygrace/?path=/story/%E6%AC%A2%E8%BF%8E%E6%9D%A5%E5%88%B0flygrace--welcome' >flygrace UI</a>
             <br/>
             <span>created by @dong ¬</span>
         </div>
