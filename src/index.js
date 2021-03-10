8d3a11ce089097893123611715ffe23d29633746
halodong <1661089448@qq.com>
2021-03-10 10:46:38 +0800

    增加更多语言

--- src/index.js
+++ src/index.js
@@ -1,11 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './App'
 import './index.css'
 import 'flygrace/dist/index.css'
-import 'codemirror/lib/codemirror.css';
-import 'codemirror/theme/nord.css';
+import 'codemirror/lib/codemirror.css'
+import 'codemirror/theme/nord.css'
 import 'codemirror/mode/javascript/javascript'
 import 'codemirror/mode/css/css'
 import 'codemirror/mode/go/go'
@@ -16,12 +15,8 @@ import 'codemirror/mode/http/http'
 import 'codemirror/mode/sql/sql'
 import 'codemirror/mode/vue/vue'
 import 'codemirror/mode/jsx/jsx'
+import 'codemirror/mode/markdown/markdown'
 
 ReactDOM.render(<App />,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
