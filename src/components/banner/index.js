b691ebd456cfe6977ad6e6ba8df7ab8c5fab8ec1
halodong <1661089448@qq.com>
2021-03-10 10:45:31 +0800

    优化

--- src/components/banner/index.js
+++ src/components/banner/index.js
@@ -3,8 +3,8 @@ export const Banner = () => {
 
     return (
         <div className='banner'>
-            {/* <a className='link' href='https://github.com/halodong/codeToImg'>github</a>
-            <a className='link' href='https://gitee.com/halodong/codeimg' >gitee</a> */}
+            <a className='link' href='https://github.com/halodong/codeImg'>github</a>
+            <a className='link' href='https://gitee.com/halodong/codeimg' >gitee</a>
             <br/>
             <span>created by @dong ¬</span>
         </div>
